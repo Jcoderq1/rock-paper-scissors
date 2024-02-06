816dadea1f329a08291a30ab168e74d725859f07
Jcoderq1 <133173596+Jcoderq1@users.noreply.github.com>
2024-02-05 23:06:59 -0500

    Create BP HTML create Rock Paper Scissors Function

---
+++ rock_paper.js
@@ -0,0 +1,7 @@
+
+// function to pick random choice between rock, paper, scissors.
+function getComputerChoice() {
+    const myArray = ['rock','paper', 'Scissors'];
+    const randomChoice = myArray[Math.floor(Math.random() * myArray.length)];
+}
+
