a5530443a6219e31b5b82c54564cf2f04bd66a04
Jcoderq1 <133173596+Jcoderq1@users.noreply.github.com>
2024-02-08 16:08:36 -0500

    create efficient checkWinner funtion/ playRound YT

--- rock_paper.js
+++ rock_paper.js
@@ -1,40 +1,126 @@
 
 // function to pick random choice between rock, paper, scissors.
 function getComputerChoice() {
-    const choices = ['rock','paper', 'Scissors'];
+    const choices = ['rock','paper', 'scissors'];
     const randomChoice = choices[Math.floor(Math.random() * choices.length)];
     return randomChoice
 };
 
-
-//let computerSelection = getComputerChoice();
-//let playerSelection = prompt("please choose rock, paper, scissors");
 function playerSelection() {
-    let playerChoice = prompt("what do you choose?")
+    let playerChoice = prompt("what do you choose?").toLocaleLowerCase();
     return playerChoice.toLocaleLowerCase()
 };
 let computerSelection = getComputerChoice();
 let playerChoice = playerSelection();
 
+// make 2 variables for keeping track of player score and computer score
+
+let playerScore = 0;
+let computerScore = 0;
+
+// create function that increases playerScore/computerScore values by 1 on each win
+
+//function increasePlayerScore() {
+    //playerScore++;
+//}
+//function increaseComputerScore() {
+   //computerScore++;
+//}
+let computerWinsRound;
+let playerWinsRound;
+
+function scoreUpPlayer() {
+    if(playerWinsRound) {
+    return playerScore+= 1;
+    }
+
+}
+function scoreUpComputer() {
+    if(computerWinsRound) {
+        return computerScore+= 1;
+     } 
+}
+//let scoreUpC = scoreUpComputer();
+//let scoreUpP = scoreUpPlayer();
+
+
 function playRound(computerSelection, playerChoice){
-    if(computerSelection === 'rock' && playerChoice === 'Scissors') {
-     console.log('You Loose! rock beats Scissors');
+    const result = checkWinner(playerChoice, computerSelection);
+    if(computerSelection === 'rock' && playerChoice === 'scissors') {
+     return (console.log('You Loose! rock beats Scissors') && scoreUpComputer());
+
     } else if(computerSelection === 'rock' && playerChoice === 'paper') {
-        console.log("You Win! paper beats rock");
-    } else if (computerSelection === 'rock' && playerChoice === 'rock') {
-        console.log("It's a Tie!");
+        return (console.log("You Win! paper beats rock") && scoreUpPlayer());
+
     } else if (computerSelection === 'paper' && playerChoice === 'rock') {
-        console.log("You loose! paper beats rock");
-    } else if(computerSelection === 'paper' && playerChoice === 'Scissors') {
-        console.log("You Win! Scissors beats paper");
-    } else if (computerSelection === 'paper' && playerChoice === 'paper') {
-        console.log("It's a Tie!");
-    } else if (computerSelection === 'Scissors' && playerChoice === 'rock') {
-        console.log("You loose! scissors beats paper");
-    } else if(computerSelection === 'Scissors' && playerChoice === 'Scissors') {
-        console.log("You Win! rock beats Scissors");
-    } else { (computerSelection === 'Scissors' && playerChoice === 'paper')
-        console.log("It's a Tie!");
+        return (console.log("You loose! paper beats rock") && scoreUpComputer());
+
+    } else if(computerSelection === 'paper' && playerChoice === 'scissors') {
+        return(console.log("You Win! scissors beats paper") && scoreUpPlayer());
+
+    } else if (computerSelection === 'scissors' && playerChoice === 'rock') {
+       return (console.log("You loose! scissors beats paper") && scoreUpComputer());
+
+    } else if(computerSelection === 'scissors' && playerChoice === 'scissors') {
+        return (console.log("You Win! rock beats Scissors") && scoreUpPlayer());
+
+    } else { console.log("It's a tie");
 } 
+    
 };
+
+//check who the winner is 
+function checkWinner(computerSelection, playerChoice) {
+    if(playerChoice == computerSelection) {
+        return "Tie";
+    }
+    else if(
+        (playerChoice == "rock" && computerSelection == "scissors") ||
+        (playerChoice == "scissors" && computerSelection == "paper") ||
+        (playerChoice == "paper" && computerSelection == "rock")
+    ){
+        return "Player"
+    } 
+    else {
+        return "Computer"
+    }
+
+}
+
 playRound(computerSelection, playerChoice);
+function currentScore() {
+    console.log("Player Score: " + playerScore);
+    console.log("Computer Score: " + computerScore);
+}
+currentScore();
+
+
+// make 2 variables for keeping track of player score and computer score
+//let playerScore = 0;
+//let computerScore = 0;
+// create function that increases playerScore/computerScore values by 1 on each win
+function increasePlayerScore() {
+    playerScore++;
+}
+function increaseComputerScore(){
+    computerScore++;
+}
+//let computerWinsRound = increaseComputerScore();
+//let playerWinsRound = increasePlayerScore();
+
+// when either score reaches five display message you win
+function currentScore() {
+    console.log("Player Score: " + playerScore);
+    console.log("Computer Score: " + computerScore);
+}
+currentScore();
+
+let cScore = currentScore();
+
+
+
+function playGame() {
+    while(cScore < 5) {
+
+    }
+}
