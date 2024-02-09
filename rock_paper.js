fd742a122aa0497a7e798e9b23351748a28d10a6
Jcoderq1 <133173596+Jcoderq1@users.noreply.github.com>
2024-02-09 10:00:48 -0500

    create game funcion to loop 5x, show score const

--- rock_paper.js
+++ rock_paper.js
@@ -6,70 +6,14 @@ function getComputerChoice() {
     return randomChoice
 };
 
-function playerSelection() {
+function getPlayerSelection() {
     let playerChoice = prompt("what do you choose?").toLocaleLowerCase();
     return playerChoice.toLocaleLowerCase()
 };
 let computerSelection = getComputerChoice();
-let playerChoice = playerSelection();
-
-// make 2 variables for keeping track of player score and computer score
-
-let playerScore = 0;
-let computerScore = 0;
-
-// create function that increases playerScore/computerScore values by 1 on each win
-
-//function increasePlayerScore() {
-    //playerScore++;
-//}
-//function increaseComputerScore() {
-   //computerScore++;
-//}
-let computerWinsRound;
-let playerWinsRound;
-
-function scoreUpPlayer() {
-    if(playerWinsRound) {
-    return playerScore+= 1;
-    }
-
-}
-function scoreUpComputer() {
-    if(computerWinsRound) {
-        return computerScore+= 1;
-     } 
-}
-//let scoreUpC = scoreUpComputer();
-//let scoreUpP = scoreUpPlayer();
+let playerChoice = getPlayerSelection();
 
 
-function playRound(computerSelection, playerChoice){
-    const result = checkWinner(playerChoice, computerSelection);
-    if(computerSelection === 'rock' && playerChoice === 'scissors') {
-     return (console.log('You Loose! rock beats Scissors') && scoreUpComputer());
-
-    } else if(computerSelection === 'rock' && playerChoice === 'paper') {
-        return (console.log("You Win! paper beats rock") && scoreUpPlayer());
-
-    } else if (computerSelection === 'paper' && playerChoice === 'rock') {
-        return (console.log("You loose! paper beats rock") && scoreUpComputer());
-
-    } else if(computerSelection === 'paper' && playerChoice === 'scissors') {
-        return(console.log("You Win! scissors beats paper") && scoreUpPlayer());
-
-    } else if (computerSelection === 'scissors' && playerChoice === 'rock') {
-       return (console.log("You loose! scissors beats paper") && scoreUpComputer());
-
-    } else if(computerSelection === 'scissors' && playerChoice === 'scissors') {
-        return (console.log("You Win! rock beats Scissors") && scoreUpPlayer());
-
-    } else { console.log("It's a tie");
-} 
-    
-};
-
-//check who the winner is 
 function checkWinner(computerSelection, playerChoice) {
     if(playerChoice == computerSelection) {
         return "Tie";
@@ -87,12 +31,49 @@ function checkWinner(computerSelection, playerChoice) {
 
 }
 
-playRound(computerSelection, playerChoice);
-function currentScore() {
-    console.log("Player Score: " + playerScore);
-    console.log("Computer Score: " + computerScore);
+function playRound(computerSelection, playerChoice){
+    const result = checkWinner(computerSelection, playerChoice);
+    if(result == "Tie") {
+     return "It's a Tie"
+
+    } 
+    else if(result == "Player") {
+        return `You Win! ${playerChoice} beats ${computerSelection}`
+
+    } else {
+        return `You loose! ${computerSelection} beats ${playerChoice}`
+    }
 }
-currentScore();
+
+function game() {
+    let computerScore = 0;
+    let playerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        const playerChoice = getPlayerSelection();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(computerSelection, playerChoice)); 
+        if(checkWinner(computerSelection, playerChoice) == "Player") {
+            playerScore++;
+        } 
+        else if(checkWinner(computerSelection, playerChoice) == "Computer") {
+            computerScore++;
+        }
+        console.log("Player Score: " + playerScore);
+        console.log("Computer Score: " + computerScore);   
+    }
+    console.log("game over")
+    if(playerScore > computerScore) {
+        console.log("Player was the winner");
+    }
+    else if(playerScore < computerScore) {
+        console.log("Computer was the winner");
+    }
+    else{ 
+        console.log("We have a Tie!");
+}
+}
+game();
+
 
 
 // make 2 variables for keeping track of player score and computer score
